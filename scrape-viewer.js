534826deda8ad9d9d40179f268e6eb3b8907a429
Martin Krošlák <kroslakma@gmail.com>
2022-04-18 09:48:34 +0200

    Change naming convention to camelCase

--- scrape-viewer.js
+++ scrape-viewer.js
@@ -5,7 +5,7 @@ const nf = new Intl.NumberFormat('en-US');
 let db = {
 	json: new Map(),
 	index: {
-		shinma_skills: null,
+		shinmaSkills: null,
 		characters: null,
 	}
 };
@@ -17,52 +17,52 @@ function datamineJsonUrl(path) {
 const en_ultimate_art_method_mst_list = datamineJsonUrl("EN/ultimate_art_method_mst_list");
 const en_character_mst_list = datamineJsonUrl("EN/character_mst_list");
 
-const weapon_map_short = [
+const weaponMapShort = [
 	'',
 	'I', 'T', 'A', 'S',
 	'B', 'H', 'P', 'P',
 ];
 
-const weapon_map_long = [
+const weaponMapLong = [
 	'',
 	'Instrument', 'Tome', 'Artifact', 'Staff',
 	'Blade', 'Heavy', 'Projectile', 'Polearm',
 ];
 
 function viewGuildHistory(historyUrl) {
-	function generateShinmaCells(shinma, shinma_skills) {
+	function generateShinmaCells(shinma, shinmaSkills) {
 		if (!shinma) {
 			return '<td align="center"></td><td align="center"></td>';
 		}
 
-		const shinma_mst = shinma_skills.get(shinma.art_mst_id);
-		const shinma_weapons =
-			weapon_map_short[shinma_mst.element1] +
-			weapon_map_short[shinma_mst.element2] +
-			weapon_map_short[shinma_mst.element3] +
-			weapon_map_short[shinma_mst.element4];
+		const shinmaMst = shinmaSkills.get(shinma.art_mst_id);
+		const shinmaWeapons =
+			weaponMapShort[shinmaMst.element1] +
+			weaponMapShort[shinmaMst.element2] +
+			weaponMapShort[shinmaMst.element3] +
+			weaponMapShort[shinmaMst.element4];
 
-		return `<td align="center">${shinma_weapons}</td>` +
+		return `<td align="center">${shinmaWeapons}</td>` +
 			`<td align="center">${shinma.guild_a_count}/${shinma.guild_b_count}</td>`;
 	}
 
-	const match_history = db.json.get(historyUrl);
-	const shinma_skills = db.index.shinma_skills;
+	const matchHistory = db.json.get(historyUrl);
+	const shinmaSkills = db.index.shinmaSkills;
 
-	let guild_a_name = match_history.items[0].guild_a_name;
+	const guildA = matchHistory.items[0].guild_a_name;
 
-	let html = `<h1>${guild_a_name}</h1>`;
+	let html = `<h1>${guildA}</h1>`;
 	html += '<table class="table table-striped table-bordered table-sm"><thead><tr>';
 	html += '<th>Date</th>';
 	html += '<th>Rank</th>';
 	html += '<th>Result</th>';
 	html += '<th>Enemy Guild</th>';
 	html += '<th>Guild ID</th>';
-	html += `<th>${guild_a_name} (Lifeforce)</th>`;
+	html += `<th>${guildA} (Lifeforce)</th>`;
 	html += '<th>Enemy Guild (Lifeforce)</th>';
-	html += `<th>${guild_a_name} (Combo)</th>`;
+	html += `<th>${guildA} (Combo)</th>`;
 	html += '<th>Enemy Guild (Combo)</th>';
-	html += `<th>${guild_a_name} (Ship)</th>`;
+	html += `<th>${guildA} (Ship)</th>`;
 	html += '<th>Enemy Guild (Ship)</th>';
 	html += '<th>1st Shinma</th>';
 	html += '<th>Tally</th>';
@@ -71,10 +71,10 @@ function viewGuildHistory(historyUrl) {
 	html += '</tr></thead>';
 	html += '<tbody>';
 
-	let match_count = match_history.items.length;
-	for (let i = match_count-1; i >= 0; i--) {
-		let m = match_history.items[i];
-		let dt = new Date(m.start_time * 1000);
+	const matchCount = matchHistory.items.length;
+	for (let i = matchCount-1; i >= 0; i--) {
+		const m = matchHistory.items[i];
+		const dt = new Date(m.start_time * 1000);
 
 		let result = "Tie";
 		if (m.guild_a_points > m.guild_b_points)
@@ -82,10 +82,10 @@ function viewGuildHistory(historyUrl) {
 		else if (m.guild_a_points < m.guild_b_points)
 			result = "Defeat";
 
-		const match_url = `?view=match&time=${m.start_time}&a=${m.guild_a_id}&b=${m.guild_b_id}`;
+		const matchUrl = `?view=match&time=${m.start_time}&a=${m.guild_a_id}&b=${m.guild_b_id}`;
 
 		html += '<tr>';
-		html += `<td><a href="${match_url}">${dt.getUTCFullYear()}/${dt.getUTCMonth()+1}/${dt.getUTCDate()}</a></td>`;
+		html += `<td><a href="${matchUrl}">${dt.getUTCFullYear()}/${dt.getUTCMonth()+1}/${dt.getUTCDate()}</a></td>`;
 		html += `<td>${m.rank}</td>`;
 		html += `<td>${result}</td>`;
 		html += `<td>${m.guild_b_name}</td>`;
@@ -96,8 +96,8 @@ function viewGuildHistory(historyUrl) {
 		html += `<td align="right">${m.guild_b_combo}</td>`;
 		html += `<td align="right">${m.guild_a_ship_win}</td>`;
 		html += `<td align="right">${m.guild_b_ship_win}</td>`;
-		html += generateShinmaCells(m.shinma[0], shinma_skills);
-		html += generateShinmaCells(m.shinma[1], shinma_skills);
+		html += generateShinmaCells(m.shinma[0], shinmaSkills);
+		html += generateShinmaCells(m.shinma[1], shinmaSkills);
 		html += '</tr>';
 	}
 	html += '</tbody></table>';
@@ -107,19 +107,19 @@ function viewGuildHistory(historyUrl) {
 }
 
 function viewMatch(dt, matchUrl) {
-	function generateShinmaCells(shinma, shinma_skills) {
+	function generateShinmaRow(shinma, shinmaSkills) {
 		if (!shinma) {
 			return '<tr><th scope="row"></th><td></td><td></td></tr>';
 		}
 
-		const shinma_mst = shinma_skills.get(shinma.artMstId);
-		const shinma_weapons =
-			weapon_map_short[shinma_mst.element1] +
-			weapon_map_short[shinma_mst.element2] +
-			weapon_map_short[shinma_mst.element3] +
-			weapon_map_short[shinma_mst.element4];
+		const shinmaMst = shinmaSkills.get(shinma.artMstId);
+		const shinmaWeapons =
+			weaponMapShort[shinmaMst.element1] +
+			weaponMapShort[shinmaMst.element2] +
+			weaponMapShort[shinmaMst.element3] +
+			weaponMapShort[shinmaMst.element4];
 
-		return `<tr><th scope="row">${shinma_weapons}</th>` +
+		return `<tr><th scope="row">${shinmaWeapons}</th>` +
 			`<td>${shinma.guildACount}</td>` +
 			`<td>${shinma.guildBCount}</td></tr>`;
 	}
@@ -133,7 +133,7 @@ function viewMatch(dt, matchUrl) {
 			const char = characters.get(m.characterMstId);
 			html += '<tr>';
 			html += `<td>${m.name}</td>`;
-			html += `<td>${weapon_map_long[char.favoriteWeapon]}</td>`;
+			html += `<td>${weaponMapLong[char.favoriteWeapon]}</td>`;
 			html += `<td>${nf.format(m.totalPower)}</td>`;
 			html += `<td>${nf.format(m.maxHp)}</td>`;
 			html += `<td>${char.name}</td>`;
@@ -144,86 +144,90 @@ function viewMatch(dt, matchUrl) {
 	}
 
 	const match = db.json.get(matchUrl);
-	const shinma_skills = db.index.shinma_skills;
+	const shinmaSkills = db.index.shinmaSkills;
 
 	let html = '<div class="container">';
 	{
 		const res = match.get_result.gvgResult;
-		const guild_a = res.selfGuildName;
-		const guild_b = res.enemyGuildName;
+		const guildA = res.selfGuildName;
+		const guildB = res.enemyGuildName;
 		const date = `${pad(4,dt.getFullYear())}/${pad(2,dt.getMonth()+1)}/${pad(2,dt.getDate())}`;
 		const time = `${dt.getHours()}:${pad(2,dt.getMinutes())}:${pad(2,dt.getSeconds())}`;
 
 		html += '<div class="row" justify-content-center>';
-		html += '<div class="col">';
-		html += `<h1>${guild_a} vs. ${guild_b}</h1>`;
-		html += `<p>${date} ${time}</p>`;
-		html += '</div>'
+		{
+			html += '<div class="col">';
+			html += `<h1>${guildA} vs. ${guildB}</h1>`;
+			html += `<p>${date} ${time}</p>`;
+			html += '</div>'
+		}
 		html += '</div>';
 
 		html += '<div class="row">';
+		{
+			html += '<div class="col">';
+			html += '<table class="table table-striped table-bordered table-sm"><thead><tr>';
+			html += '<th></th>';
+			html += `<th scope="col">${guildA}</th>`;
+			html += `<th scope="col">${guildB}</th>`;
+			html += '</tr></thead>';
+			html += '<tbody>';
 
-		html += '<div class="col">';
-		html += '<table class="table table-striped table-bordered table-sm"><thead><tr>';
-		html += '<th></th>';
-		html += `<th scope="col">${guild_a}</th>`;
-		html += `<th scope="col">${guild_b}</th>`;
-		html += '</tr></thead>';
-		html += '<tbody>';
+			const lifeforceA = nf.format(res.selfTotalGuildPoint);
+			const lifeforceB = nf.format(res.enemyTotalGuildPoint);
+			html += `<tr><th scope="row">Lifeforce</th><td>${lifeforceA}</td><td>${lifeforceB}</td></tr>`;
 
-		const lifeforce_a = nf.format(res.selfTotalGuildPoint);
-		const lifeforce_b = nf.format(res.enemyTotalGuildPoint);
-		html += `<tr><th scope="row">Lifeforce</th><td>${lifeforce_a}</td><td>${lifeforce_b}</td></tr>`;
+			const combo_a = nf.format(res.selfComboCount);
+			const combo_b = nf.format(res.enemyComboCount);
+			html += `<tr><th scope="row">Combo</th><td>${combo_a}</td><td>${combo_b}</td></tr>`;
 
-		const combo_a = nf.format(res.selfComboCount);
-		const combo_b = nf.format(res.enemyComboCount);
-		html += `<tr><th scope="row">Combo</th><td>${combo_a}</td><td>${combo_b}</td></tr>`;
+			const shipsA = nf.format(res.selfSiegeWinCount);
+			const shipsB = nf.format(res.enemySiegeWinCount);
+			html += `<tr><th scope="row">Downed ships</th><td>${shipsA}</td><td>${shipsB}</td></tr>`;
+			html += '</tbody></table>';
+			html += '</div>'
+		}
 
-		const ships_a = nf.format(res.selfSiegeWinCount);
-		const ships_b = nf.format(res.enemySiegeWinCount);
-		html += `<tr><th scope="row">Downed ships</th><td>${ships_a}</td><td>${ships_b}</td></tr>`;
-		html += '</tbody></table>';
-		html += '</div>'
-
-		const shinma = match.get_result.gvgUltimateArtInfo.gvgUltimateArtDataList;
-		html += '<div class="col">';
-		html += '<table class="table table-striped table-bordered table-sm"><thead><tr>';
-		html += '<th>Shinma</th>';
-		html += `<th scope="col">${guild_a}</th>`;
-		html += `<th scope="col">${guild_b}</th>`;
-		html += '</tr></thead>';
-		html += '<tbody>';
-		html += generateShinmaCells(shinma[0], shinma_skills);
-		html += generateShinmaCells(shinma[1], shinma_skills);
-		html += '</tbody></table>';
-		html += '</div>'
-
+		{
+			const shinma = match.get_result.gvgUltimateArtInfo.gvgUltimateArtDataList;
+			html += '<div class="col">';
+			html += '<table class="table table-striped table-bordered table-sm"><thead><tr>';
+			html += '<th>Shinma</th>';
+			html += `<th scope="col">${guildA}</th>`;
+			html += `<th scope="col">${guildB}</th>`;
+			html += '</tr></thead>';
+			html += '<tbody>';
+			html += generateShinmaRow(shinma[0], shinmaSkills);
+			html += generateShinmaRow(shinma[1], shinmaSkills);
+			html += '</tbody></table>';
+			html += '</div>'
+		}
 
 		html += '</div>';
 	}
 
 	{
-		const rank_types = [
+		const rankTypes = [
 			"Lifeforce", "Recover",
 			"Ally ATK Support", "Ally DEF Support",
 			"Enemy ATK Debuff", "Enemy DEF Debuff",
 			"Combo"
 		];
-		const mvp_ranks = match.get_result.gvgMvpRank;
+		const mvpRanks = match.get_result.gvgMvpRank;
 		html += '<div class="row">';
 		html += '<h2>Contribution Rank</h2>';
-		for (let i = 0; i < mvp_ranks.length; i++)
+		for (let i = 0; i < mvpRanks.length; i++)
 		{
-			const mvp_rank = mvp_ranks[i];
+			const mvpRank = mvpRanks[i];
 			html += '<div class="col-md-4">';
-			html += '<div class="card">'
-			html += '<div class="card-body">'
-			html += `<h3 class="card-title">${rank_types[mvp_rank.type-1]}</h3>`;
+			html += '<div class="card">';
+			html += '<div class="card-body">';
+			html += `<h3 class="card-title">${rankTypes[mvpRank.type - 1]}</h3>`;
 			html += '<table class="table table-striped table-bordered table-sm"><tbody>';
-			const list = mvp_rank.gvgMvpRankDetailList;
+			const list = mvpRank.gvgMvpRankDetailList;
 			for (let j = 0; j < list.length; j++) {
 				html += '<tr class="text-center">';
-				html += `<td rowspan="2" class="align-middle">${j+1}</td>`;
+				html += `<td rowspan="2" class="align-middle">${j + 1}</td>`;
 				html += `<td>${list[j].nameA}</td>`;
 				html += `<td>${list[j].nameB}</td>`;
 				html += '</tr>';
@@ -292,13 +296,13 @@ async function updateView() {
 		break;
 
 		case "match":
-			const start_time = parseInt(params.get('time'));
-			const guild_a = parseInt(params.get('a'));
-			const guild_b = parseInt(params.get('b'));
-			if (!start_time || !guild_a || !guild_b)
+			const startTime = parseInt(params.get('time'));
+			const guildA = parseInt(params.get('a'));
+			const guildB = parseInt(params.get('b'));
+			if (!startTime || !guildA || !guildB)
 				return;
 
-			const dt = new Date(start_time * 1000);
+			const dt = new Date(startTime * 1000);
 
 			let match_url = 'matches/';
 			match_url += pad(4, dt.getUTCFullYear());
@@ -312,7 +316,7 @@ async function updateView() {
 			match_url += pad(2, dt.getUTCMinutes());
 			match_url += '-';
 			match_url += pad(2, dt.getUTCSeconds());
-			match_url += `_${pad(5, guild_a)}_vs_${pad(5, guild_b)}.json`;
+			match_url += `_${pad(5, guildA)}_vs_${pad(5, guildB)}.json`;
 
 			await Promise.allSettled([
 				cacheJson(match_url, match_url),
@@ -359,21 +363,21 @@ async function cacheJson(url) {
 }
 
 async function cacheShinmaSkills() {
-	if (db.index.shinma_skills) {
+	if (db.index.shinmaSkills) {
 		return;
 	}
 
 	await cacheJson(en_ultimate_art_method_mst_list);
 
-	const shinma_table = db.json.get(en_ultimate_art_method_mst_list);
+	const shinmaTable = db.json.get(en_ultimate_art_method_mst_list);
 
-	let shinma_skills = new Map();
-	for (let i = 0; i < shinma_table.length; i++) {
-		let shinma_mst = shinma_table[i];
-		shinma_skills.set(shinma_mst.ultimateArtMethodMstId, shinma_mst);
+	let shinmaSkills = new Map();
+	for (let i = 0; i < shinmaTable.length; i++) {
+		let shinmaMst = shinmaTable[i];
+		shinmaSkills.set(shinmaMst.ultimateArtMethodMstId, shinmaMst);
 	}
 
-	db.index.shinma_skills = shinma_skills;
+	db.index.shinmaSkills = shinmaSkills;
 }
 
 async function cacheCharacters() {
